5ca1f8dd9b6bc63c6318f77d83e3934efc54a7e1
ZAII <ivy2846357@icloud.com>
2022-02-22 21:58:31 +0800

    新增登入註冊狀態JS效果

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,27 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+/*------------------- 登入註冊按鈕切換效果 -------------------*/
+
+$(document).ready(function () {
+
+  $('.loginBtn').click(function (e) {
+
+    /*----------- 底線位置移動 -----------*/
+    $('.signForm__enableLine').removeClass('signForm__enableLine--register').addClass('signForm__enableLine--login');
+
+    /*----------- 移除註冊特效 -----------*/
+    $('.registerBtn').removeClass('fw-bold').removeClass('text-primary').addClass('text-inactive');
+    /*----------- 新增登入特效 -----------*/
+    $('.loginBtn').addClass('fw-bold').addClass('text-primary');
+  })
+
+  $('.registerBtn').click(function (e) {
+
+    /*----------- 底線位置移動 -----------*/
+    $('.signForm__enableLine').removeClass('signForm__enableLine--login').addClass('signForm__enableLine--register');
+
+    /*----------- 移除登入特效 -----------*/
+    $('.loginBtn').removeClass('fw-bold').removeClass('text-primary').addClass('text-inactive');
+    /*----------- 新增註冊特效 -----------*/
+    $('.registerBtn').addClass('fw-bold').addClass('text-primary');
+  })
+
+})
