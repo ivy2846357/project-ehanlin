60044c0d6bdbcca1f3d8ef35092ab38c8860e8cf
ZAII <ivy2846357@icloud.com>
2022-02-22 22:08:32 +0800

    新增登入表單及切換JS效果

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,6 +11,10 @@ $(document).ready(function () {
     $('.registerBtn').removeClass('fw-bold').removeClass('text-primary').addClass('text-inactive');
     /*----------- 新增登入特效 -----------*/
     $('.loginBtn').addClass('fw-bold').addClass('text-primary');
+
+    /*----------- 切換登入表單 -----------*/
+    $('.loginForm').addClass('d-block').removeClass('d-none');
+    $('.registerForm').addClass('d-none').removeClass('d-block');
   })
 
   $('.registerBtn').click(function (e) {
@@ -22,6 +26,10 @@ $(document).ready(function () {
     $('.loginBtn').removeClass('fw-bold').removeClass('text-primary').addClass('text-inactive');
     /*----------- 新增註冊特效 -----------*/
     $('.registerBtn').addClass('fw-bold').addClass('text-primary');
+
+    /*----------- 切換註冊表單 -----------*/
+    $('.loginForm').addClass('d-none').removeClass('d-block');
+    $('.registerForm').addClass('d-block').removeClass('d-none');
   })
 
 })
