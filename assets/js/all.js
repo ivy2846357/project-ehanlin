7e7e1f17ebedae5adbf9a098effd0a19bd31c912
ZAII <ivy2846357@icloud.com>
2022-02-23 18:52:25 +0800

    Update 2022-02-23T10:52:21.423Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -35,6 +35,12 @@ $(document).ready(function () {
 
   $('.navbar__hamburger').click(function (e) {
     $('.navbar__hamburgerIcon').toggleClass('navbar__hamburgerIcon--change');
+  });
+  /*----------- 下拉選單箭頭轉換 -----------*/
+
+  $('.dropdown__link').click(function (e) {
+    $(this).find('.dropdown__arrow').toggleClass('dropdown__arrow--change');
+    $(this).parent().siblings().find('.dropdown').addClass('add');
   }); // $('.dropdown').click(function (e) {
   //   $(this).addClass('dropdown__link--change');
   //   $(this).parent().siblings().find('dropdown').removeClass('dropdown__link--change');
